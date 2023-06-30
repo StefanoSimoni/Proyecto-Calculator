0e23d2d77a110441c055aacf2febbf30f443f303
Your Name <stefano.simoni.10@hotmail.com.ar>
2023-06-30 15:42:49 -0300

    Commit 3
    Fix de errores en la calculadora. La calculadora es capaz de realizar operaciones basicas de un par de numeros.

--- main.js
+++ main.js
@@ -5,35 +5,37 @@ const fillScreen = () => {
     let firstNumber = 0
     let operator = ""
     let secondNumber = 0
-    let asd = []
+    let numbers = []
     keys.forEach((key) => {
         key.addEventListener("click", () => {
             if((key.textContent != "C") && (key.textContent != "=")) {
                 if((key.textContent != "+") && (key.textContent != "-") && (key.textContent != "x") && (key.textContent != "/")) {
-                    asd.push(key.textContent)
-                    resultScreen.textContent = asd.join("")
+                    numbers.push(key.textContent)
+                    resultScreen.textContent = numbers.join("")
+                    if(firstNumber != 0) {
+                        secondNumber = parseInt(numbers.join(""))
+                        numbers = []
+                    }
                 } else {
                     if(firstNumber == 0) {
-                        firstNumber = parseInt(asd.join(""))
+                        firstNumber = parseInt(numbers.join(""))
                         resultScreen.innerHTML = ""
-                        asd = []
+                        numbers = []
                         operator = key.textContent
-                    } else {
-                        secondNumber = parseInt(asd.join(""))
-                        asd = []
                     }
                 }
             } else if(key.textContent == "C") {
                 resultScreen.innerHTML = ""
-                asd = []
+                numbers = []
                 firstNumber = 0
                 operator = ""
                 secondNumber = 0
             } else if(key.textContent == "=") {
                 resultScreen.innerHTML = ""
-                asd = []
-                resultScreen.textContent = operate(firstNumber, operator, secondNumber)
-                firstNumber = 0
+                numbers = []
+                let operation = operate(firstNumber, operator, secondNumber)
+                resultScreen.textContent = operation
+                firstNumber = operation
                 operator = ""
                 secondNumber = 0
             }
@@ -63,7 +65,7 @@ const operate = (firstNumber, operator, secondNumber) => {
             return sum(firstNumber, secondNumber)
         case "-":
             return minus(firstNumber, secondNumber)
-        case "*":
+        case "x":
             return multiply(firstNumber, secondNumber)
         case "/":
             return divide(firstNumber, secondNumber)
@@ -72,5 +74,4 @@ const operate = (firstNumber, operator, secondNumber) => {
     }
 }
 
-operate(1, "+", 1)
 fillScreen()
